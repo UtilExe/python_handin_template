543b338b07d718d49c8c709fa0b65ad035758751
UtilExe <laymasterlolz@gmail.com>
2021-02-14 18:36:40 +0100

    All exercises completed.

--- modules/utils.py
+++ modules/utils.py
@@ -15,19 +15,41 @@ def get_all_file_names(folderpath,out='python_handin_template/output2.txt'):
             for name in files:
                 file_object.write(str(name) + '\n')
 
-# not sure if this is the right way. using the * abritary function parameter.
+# not sure if this is the right way. using the * abritary function parameter. not sure if there's a smarter way?
 def print_line_one(*file_names):
     """takes a list of filenames and print the first line of each"""
     lines = []
     for file in file_names:
         with open(file) as file_object:
-            lines.append("File: " + file +": " + file_object.readline())
+            lines.append("File: " + file +": " + file_object.readline()) #readline handles to read only first line
     
     for line in lines:
         print(line.rstrip())
 
-def print_emails(file_names):
+# not sure if this is the right way. using the * abritary function parameter. not sure if there's a smarter way?
+def print_emails(*file_names):
     """takes a list of filenames and print each line that contains an email (just look for @)"""
+    lines = []
+    for file in file_names:
+        with open(file) as file_object:
+            line = file_object.readlines();
+            for entry in line:
+                if ('@' in entry):
+                    lines.append("File: " + file +": " + entry)
 
-def write_headlines(md_files, out='output.txt'):
+    for line in lines:
+        print(line.rstrip())
+
+def write_headlines(*md_files, out='python_handin_template/output3.txt'):
     """takes a list of md files and writes all headlines (lines starting with #) to a file"""
+    lines = []
+    for file in md_files:
+        with open(file) as file_object:
+            line = file_object.readlines();
+            for entry in line:
+                if ('#' in entry):
+                    lines.append("File: " + file +": " + entry)
+        with open(out, 'w') as file_object:
+            for field in lines:
+               # print("test", field)
+                file_object.write(str(field))
